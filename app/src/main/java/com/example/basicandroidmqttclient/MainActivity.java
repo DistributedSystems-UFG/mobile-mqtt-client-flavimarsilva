42d8d8ac734f365884dbdea760e6f46349d9d281
flavimarsilva <79484934+flavimarsilva@users.noreply.github.com>
2023-12-11 00:56:25 -0300

    Finalização da tarefa

--- app/src/main/java/com/example/basicandroidmqttclient/MainActivity.java
+++ app/src/main/java/com/example/basicandroidmqttclient/MainActivity.java
@@ -18,25 +18,26 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.basicandroidmqttclient.MESSAGE";
-    public static final String brokerURI = "3.223.10.115";
+    public static final String brokerURI = "54.147.171.16";
 
     Activity thisActivity;
-    TextView subMsgTextView;
-
+    TextView averageMsgTextView;
+    TextView quantMsgTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         thisActivity = this;
-        subMsgTextView = (TextView) findViewById(R.id.editTextMultiLineSubMsg);
+        averageMsgTextView = (TextView) findViewById(R.id.valueAverage);
+        quantMsgTextView = (TextView) findViewById(R.id.valueQuant);
     }
 
     /** Called when the user taps the Send button */
     public void publishMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText topicName = (EditText) findViewById(R.id.editTextTopicName);
-        EditText value = (EditText) findViewById(R.id.editTextValue);
+        String topicName = "temp";
+        EditText value = (EditText) findViewById(R.id.valueTemp);
 
         Mqtt5BlockingClient client = Mqtt5Client.builder()
                 .identifier(UUID.randomUUID().toString())
@@ -45,20 +46,20 @@ public class MainActivity extends AppCompatActivity {
 
         client.connect();
         client.publishWith()
-                .topic(topicName.getText().toString())
+                .topic(topicName)
                 .qos(MqttQos.AT_LEAST_ONCE)
                 .payload(value.getText().toString().getBytes())
                 .send();
         client.disconnect();
 
-        String message = topicName.getText().toString() + " " + value.getText().toString();
+        String message = topicName + " " + value.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 
     public void sendSubscription(View view) {
-        EditText topicName = (EditText) findViewById(R.id.editTextTopicNameSub);
-
+        String topicAverage = "media";
+        String topicQuant = "quant";
         Mqtt5BlockingClient client = Mqtt5Client.builder()
                 .identifier(UUID.randomUUID().toString())
                 .serverHost(brokerURI)
@@ -68,12 +69,23 @@ public class MainActivity extends AppCompatActivity {
 
         // Use a callback lambda function to show the message on the screen
         client.toAsync().subscribeWith()
-                .topicFilter(topicName.getText().toString())
+                .topicFilter(topicAverage)
                 .qos(MqttQos.AT_LEAST_ONCE)
                 .callback(msg -> {
                     thisActivity.runOnUiThread(new Runnable() {
                         public void run() {
-                            subMsgTextView.setText(new String(msg.getPayloadAsBytes(), StandardCharsets.UTF_8));
+                            averageMsgTextView.setText(new String(msg.getPayloadAsBytes(), StandardCharsets.UTF_8));
+                        }
+                    });
+                })
+                .send();
+        client.toAsync().subscribeWith()
+                .topicFilter(topicQuant)
+                .qos(MqttQos.AT_LEAST_ONCE)
+                .callback(msg -> {
+                    thisActivity.runOnUiThread(new Runnable() {
+                        public void run() {
+                            quantMsgTextView.setText(new String(msg.getPayloadAsBytes(), StandardCharsets.UTF_8));
                         }
                     });
                 })
